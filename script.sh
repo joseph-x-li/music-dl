369e484bd968923eb7fab97c23dd15662040bb5d
Joseph Li <13826851+joseph-x-li@users.noreply.github.com>
2022-01-12 16:41:27 -0800

    does any  of thiws worka sdflkjasl;dkfj;asdf

--- script.sh
+++ script.sh
@@ -3,29 +3,44 @@
 echo "Welcome to music-dl. Enter in a youtube playlist URL:"
 read playlisturl
 
-echo "Enter in the name of the playlist (typically Music ##):"
-read albumname
+# split playlisturl by '=' and save the second half as playlistid
+playlistid=$(echo $playlisturl | cut -d '=' -f 2)
+
+# print out the playlistid
+echo "Playlist ID: $playlistid"
 
 mkdir -p downloads
 cd downloads
 
-if [ -d "dltmp" ]
-then 
-    rm -rf dltmp
-fi 
+# if playlistid is a folder in the current directory:
+# 1) go into the folder
+# 2) read the only folder in the current folder and save its name as playlistname
+# else if playlistid is not a folder in the current directory:
+# run youtube-dl but send SIGINT after 1 second
+if [ ! -d "$playlistid" ]; then
+    timeout 3s youtube-dl --format "bestaudio[ext=m4a]"\
+        -o "%(playlist_id)s/%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s"\
+        --add-metadata\
+        --postprocessor-args "-metadata artist=Youtube -metadata album=%(playlist_title)s"\
+        --embed-thumbnail\
+        $playlisturl
+fi
 
-mkdir dltmp && cd dltmp
+# cd into playlistid folder
+cd $playlistid
 
+# read the playlistname 
+playlistname=$(ls)
+
+cd ..
 youtube-dl --format "bestaudio[ext=m4a]"\
-    -o "$albumname/%(playlist_index)s - %(title)s.%(ext)s"\
+    -o "%(playlist_id)s/%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s"\
     --add-metadata\
-    --postprocessor-args "-metadata artist=Youtube -metadata album=\"$albumname\""\
-    --embed-thumbnail $playlisturl
+    --postprocessor-args "-metadata artist=Youtube -metadata album=\"$playlistname\""\
+    --embed-thumbnail\
+    $playlisturl
 
-mv ./* .. # move out of dltmp
-cd .. # exit dltmp
-rmdir dltmp # remove dltmp (now empty)
-cd "$albumname" # open new album
+cd $playlistid
 
 # Import into Apple's magical music ecosystem and sync with phone.
 open -a Music.app *
